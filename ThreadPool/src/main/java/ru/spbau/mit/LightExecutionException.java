f0aba0a63afde465b414f04b1e247c264e0af165
Angelika <anzhelikamukhina@yandex.ru>
2017-10-17 23:06:30 +0300

    odd files

--- ThreadPool/src/main/java/ru/spbau/mit/LightExecutionException.java
+++
@@ -1,7 +0,0 @@
-package ru.spbau.mit;
-
-public class LightExecutionException extends Exception {
-    LightExecutionException(Throwable t) {
-        super(t);
-    }
-}
--- ThreadPool/src/main/java/ru/spbau/mit/ThreadPool.java
+++
@@ -1,9 +0,0 @@
-package ru.spbau.mit;
-
-import java.util.function.Supplier;
-
-public interface ThreadPool {
-    <RetType> LightFuture<RetType> submit(Supplier<RetType> supplier);
-
-    void shutdown();
-}
